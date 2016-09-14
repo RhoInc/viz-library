22a5a233b337b04aefddcbd62a0939a314853b03
Becca Zabel <rebecca_zabel@rhoworld.com>
2016-09-14 15:17:44 -0400

    Create dotplot examples

---
+++ R-examples/002-dotplot-ggplot/002-dotplot-ggplot.R
@@ -0,0 +1,51 @@
+###########################################################################
+#
+# title: "R Example 002: Discrete data dotplot using ggplot2"
+# author: "Becca Krouse"
+# date: "September 14, 2016"
+#
+###########################################################################
+
+# load packages
+install.packages('pacman')
+pacman::p_load(dplyr, tidyr, ggplot2)
+
+# read in data
+data <- read.csv('./data/discrete_scores.csv')
+
+# create column containing median score for each category
+data <- data %>% 
+  group_by(category) %>% 
+  mutate(m=median(score),
+         lab=c('gray60','red')[I(score==m)+1])
+
+
+# plot function
+png(file='./R-examples/002-dotplot-ggplot/002-dotplot-ggplot.png',height = 8, width = 8, units = 'in', res = 300)
+
+ggplot(data, aes(x=score)) +
+  geom_dotplot(aes(x=score), fill="gray30", color="gray30",stackdir='center', binwidth=1, dotsize=1.1, stackratio=1.5) +
+  facet_wrap(~category, ncol=1, scales='free_x') +
+  theme_bw() +
+  scale_x_continuous(minor_breaks=seq(0,100,5), breaks = seq(0, 100, 20), limits=c(0,100)) +
+  geom_vline(aes(xintercept = m, color='red'), size=1, data=data) +
+  theme(
+    strip.text = element_text(size=12),
+    strip.text = element_text(size=12),
+    plot.background = element_blank(),
+    panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank(),
+    panel.grid.major.x = element_line(color='gray60'), 
+    panel.grid.minor.x = element_blank(), 
+    panel.border = element_blank(),
+    axis.title=element_blank(),
+    axis.text.y=element_blank(),
+    axis.ticks.y=element_blank(),
+    axis.ticks.x=element_blank(),
+    axis.text.x=element_text(color='gray60'),
+    strip.background = element_rect(fill = 'gray90', color=NA),
+    legend.position = "none"
+  )
+
+
+dev.off()
---
+++ R-examples/002-dotplot-lattice/002-dotplot-lattice.R
@@ -0,0 +1,53 @@
+###########################################################################
+#
+# title: "R Example 002: Discrete data dotplot using Lattice"
+# author: "Becca Krouse"
+# date: "September 14, 2016"
+#
+###########################################################################
+
+# load packages
+install.packages('pacman')
+pacman::p_load(dplyr, tidyr, lattice, latticeExtra, HH)
+
+# read in data
+data <- read.csv('./data/discrete_scores.csv')
+
+# define function to customize vertical lines & associated axis tick labels
+# by coloring median values red, and the rest gray
+panel.dotplot_highlightMedian <- function(x, l){  # l = vector of axis tick values
+  
+  panel.segments(l,0.6,l,1.5,col="gray70")          
+  panel.text(l,0.55,l,col="gray70",cex=0.7)
+  
+  panel.segments(quantile(x,0.5,type=4),0.6,quantile(x,0.5,type=4),1.5,col="red")
+  panel.text(quantile(x,0.5,type=4),0.55,quantile(x,0.5,type=4),col="red",cex=0.7)
+}
+
+# plot function
+png(file='./R-examples/002-dotplot-lattice/002-dotplot-lattice.png',height = 8, width = 8, units = 'in', res = 300)
+
+dotplot(~score|category, 
+        data=data, 
+        col.symbol="gray50",
+        asp=0.2, layout=c(1,4),as.table=T,
+        ylab=" ", xlab="", 
+        scales=list(x = list(draw=F), y=list(draw=F)),
+        par.strip.text = list(cex=0.8),
+        par.settings=list(strip.background=list(col="gray90"),
+                          axis.line=list(col='transparent'),
+                          strip.border=list(col='transparent')),
+        main="Discrete Scores by Category",
+        prepanel=function(...) list(xlim=c(0,100)),
+        panel=function(x,y,...){
+          
+          # highlight median values red, while the rest of the lines/values are gray
+          l <- seq(0,100,20)
+          panel.dotplot_highlightMedian(x,l)
+          
+          # Plot dotplot - multiple hits on a specific x value are stacked
+          HH::panel.dotplot.tb(x,y,factor=0.5,col.line="transparent",...)
+        }                    
+)
+
+dev.off()
