a9a86fb3a1e605b9726128c429b7434b2832b9ca
Becca Krouse <rebecca_zabel@rhoworld.com>
2016-12-16 16:58:19 -0500

    update R fig template

--- templates/template_R.R
+++ templates/template_R.R
@@ -8,7 +8,6 @@
 #' features:
 #' ---
 
-pacman::p_load(ggplot2)
 
 new_fig_dir <- "path"
 new_fig_name <- "0001-new-fig"
@@ -18,11 +17,13 @@ new_fig_name <- "0001-new-fig"
 #      and output as PNG for last step ***
 
 # Figure code
+pacman::p_load(ggplot2)
+
 p <- ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) +
   geom_point()
 
 # Save image of figure:
-png(filename = paste0(new_fig_dir,'/',new_fig_name, ".png"), width = 6, height = 5)
+png(filename = paste0(new_fig_dir,'/',new_fig_name, ".png"), width = 6, height = 5, units='in', res=300)
 p
 dev.off()
 
