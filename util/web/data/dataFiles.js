fde8a8dd3c8caae502a63f8d8eaa5509734b3de6
Jeremy Wildfire <jwildfire@gmail.com>
2017-05-18 18:49:31 -0700

    add screenshot for example 14

--- util/web/data/dataFiles.js
+++ util/web/data/dataFiles.js
@@ -48,6 +48,22 @@ var dataFiles = [
     cols: 11
   },
   {
+    local_path: "/Users/jwildfire/Sites/github/viz-library/data/safetyData-queries/ADAE.csv",
+    filename: "ADAE.csv",
+    ext: "csv",
+    rel_path: "./safetyData-queries/ADAE.csv",
+    rows: 402,
+    cols: 23
+  },
+  {
+    local_path: "/Users/jwildfire/Sites/github/viz-library/data/safetyData-queries/ADBDS.csv",
+    filename: "ADBDS.csv",
+    ext: "csv",
+    rel_path: "./safetyData-queries/ADBDS.csv",
+    rows: 22695,
+    cols: 20
+  },
+  {
     local_path: "/Users/jwildfire/Sites/github/viz-library/data/safetyData/ADAE.csv",
     filename: "ADAE.csv",
     ext: "csv",
@@ -126,21 +142,5 @@ var dataFiles = [
     rel_path: "./safetyData/vitalSigns.csv",
     rows: 27,
     cols: 7
-  },
-  {
-    local_path: "/Users/jwildfire/Sites/github/viz-library/data/safetyData-queries/ADAE.csv",
-    filename: "ADAE.csv",
-    ext: "csv",
-    rel_path: "./safetyData-queries/ADAE.csv",
-    rows: 402,
-    cols: 23
-  },
-  {
-    local_path: "/Users/jwildfire/Sites/github/viz-library/data/safetyData-queries/ADBDS.csv",
-    filename: "ADBDS.csv",
-    ext: "csv",
-    rel_path: "./safetyData-queries/ADBDS.csv",
-    rows: 22695,
-    cols: 20
   }
 ];
