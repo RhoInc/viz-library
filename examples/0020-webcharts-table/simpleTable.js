eacd3a8cd4b9aff55c57490516177f4726672654
Jeremy Wildfire <jwildfire@gmail.com>
2017-09-19 08:16:35 -0400

    added webcharts table example

---
+++ examples/0020-webcharts-table/simpleTable.js
@@ -0,0 +1,35 @@
+var settings = {
+  "pagination":true,
+  "nRowsPerPage":10,
+  "nPageLinksDisplayed":10
+};
+
+var controls = webCharts.createControls(".chart",	{
+		location: 'top',
+		inputs:[
+      {type: "subsetter", value_col: "Period", label: "Filter by Period"},
+      {type: "subsetter", value_col: "Group", label: "Filter by Group"}
+		]
+	})
+
+d3.csv("../../data/elements.csv", function(error, data) {
+  var table = webCharts.createTable(".chart", settings,controls);
+  table.init(data);
+
+  d3.selectAll(".controls input").on("change",function(){
+    settings.pagination = d3.select("input.pagination").property("checked")
+    settings.nRowsPerPage = d3.select("input.items").node().value
+    settings.nPageLinksDisplayed = d3.select("input.pages").node().value
+    console.log(settings)
+    d3.select(".chart").selectAll("*").remove()
+    var controls = webCharts.createControls(".chart",	{
+    		location: 'top',
+    		inputs:[
+          {type: "subsetter", value_col: "Period", label: "Filter by Period"},
+          {type: "subsetter", value_col: "Group", label: "Filter by Group"}
+    		]
+    	})
+    var table = webCharts.createTable(".chart", settings, controls);
+    table.init(data);
+  })
+})
