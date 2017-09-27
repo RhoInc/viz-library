a633677f953f28132e8f896cb9b5d775f0545f16
Spencer Childress <samussiah@gmail.com>
2017-09-27 14:59:23 -0400

    add new functionality to table example

--- examples/0020-webcharts-table/simpleTable.js
+++ examples/0020-webcharts-table/simpleTable.js
@@ -1,8 +1,11 @@
 var settings = {
+  "sortable":true,
+  "searchable":true,
+  "exportable":true,
+  "exports": ['csv', 'xlsx'],
   "pagination":true,
   "nRowsPerPage":10,
-  "nPageLinksDisplayed":5,
-  "searchable":true
+  "nPageLinksDisplayed":5
 };
 
 var controls = webCharts.createControls(".chart",	{
@@ -18,14 +21,16 @@ d3.csv("../../data/elements.csv", function(error, data) {
   table.init(data);
 
   d3.selectAll(".controls input").on("change",function(){
+    settings.sortable = d3.select("input.sortable").property("checked")
+    settings.searchable = d3.select("input.searchable").property("checked")
+    settings.exportable = d3.select("input.exportable").property("checked")
     settings.pagination = d3.select("input.pagination").property("checked")
     settings.nRowsPerPage = +d3.select("input.items").node().value
     settings.nPageLinksDisplayed = +d3.select("input.pages").node().value
     settings.applyCSS = d3.select("input.applyCSS").property("checked")
-    settings.searchable = d3.select("input.searchable").property("checked")
-    settings.sort = d3.select("input.sort").property("checked")
 
     console.log(settings)
+
     d3.select(".chart").selectAll("*").remove()
     var controls = webCharts.createControls(".chart",	{
     		location: 'top',
