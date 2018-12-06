8b7b8d2ca79320623a2cb728282eae2c914ed48a
samussiah <samussiah@gmail.com>
2018-12-06 16:04:06 -0500

    update queries data

--- data/dataCleaning/queries/create_queries.R
+++ data/dataCleaning/queries/create_queries.R
@@ -8,90 +8,118 @@ nQueries <- 5000
 
     forms <- read.csv('forms.csv', colClasses = 'character', check.names = FALSE)
     fields <- read.csv('fields.csv', colClasses = 'character', check.names = FALSE)
-    statuses <- c('Open', 'Closed', 'Answered', 'Cancelled')
-    statusProbs <- c(.1, .7, .1, .1)
+    statuses <- c('Closed', 'Cancelled') # Open and Answered defined by query answered/resolved dates or lack thereof
+    statusProbs <- c(.75, .25)
     markingGroups <- c('Site from System', 'Site from DM', 'Site from CRA')
     markingGroupProbs <- c(.7, .2, .1)
     sites <- 1:5
     visits <- 1:10
-    queryDates <- seq(as.Date('2015-01-01'), as.Date('2016-12-31'), 1)
+    snapshotDate <- as.Date('2016-12-31')
+    queryDates <- seq(as.Date('2015-01-01'), snapshotDate, 1)
+    responses <- runif(nQueries)
+    resolvers <- runif(nQueries)
 
 #-------------------------------------------------------------------------------------------------#
 # Sample fields
 #-------------------------------------------------------------------------------------------------#
 
     queries <- data.frame(
-            `formoid` = rep('', nQueries), # 1
-            `fieldname` = rep('', nQueries), # 2
-            `querystatus` = rep('', nQueries), # 3
-            `markinggroup` = rep('', nQueries), # 4
-            `sitename` = rep('', nQueries), # 5
-            `subjectnameoridentifier ` = rep('', nQueries), # 6
-            `folderoid` = rep('', nQueries), # 7
-            `queryopendt` = rep(Sys.Date(), nQueries), # 8
-            `queryresolvedt` = rep(Sys.Date(), nQueries), # 9
-            `qdays` = rep(0, nQueries), # 10
-            `queryage` = rep('', nQueries), # 11 - also derived in renderer
-            `rdays` = rep(0, nQueries), # 12
-            `queryrecency` = rep(0, nQueries), # 13 - also derived in renderer
+            sitename = rep('', nQueries),
+            subjectnameoridentifier  = rep('', nQueries),
+            folderoid = rep('', nQueries),
+            formoid = rep('', nQueries),
+            fieldname = rep('', nQueries),
+            markinggroup = rep('', nQueries),
+            queryopendt = rep(Sys.Date(), nQueries),
+            queryresponsedt = rep(Sys.Date(), nQueries),
+            queryresolveddt = rep(Sys.Date(), nQueries),
+            querystatus = rep('', nQueries),
+            odays = rep(0, nQueries),
+            queryrecency = rep(0, nQueries), # also derived in renderer
+            qdays = rep(0, nQueries),
+            queryage = rep('', nQueries), # also derived in renderer
         stringsAsFactors = FALSE,
         check.names = FALSE
     )
 
     for (i in 1:nQueries) {
         query <- fields[sample(nrow(fields), 1),] # no need for select() with -Field because data no longer contains a field var for explaining field
-        queries[i,1] <- query[1,1]
-        queries[i,2] <- query[1,2]
-        queries[i,3] <- sample(statuses, 1, prob = statusProbs)
-        queries[i,4] <- sample(markingGroups, 1, prob = markingGroupProbs)
-        queries[i,5] <- paste(
+        queries[i,'sitename'] <- paste(
             'Site',
             formatC(sample(sites, 1), width = 2, format = 'd', flag = '0')
         )
-        queries[i,6] <- paste(
-            strsplit(queries[i,5], ' ')[[1]][[2]],
+        queries[i,'subjectnameoridentifier'] <- paste(
+            strsplit(queries[i,'sitename'], ' ')[[1]][[2]],
             formatC(sample(1:25, 1), width = 3, format = 'd', flag = '0'),
             sep = '-'
         )
-        queries[i,7] <- paste(
+        queries[i,'folderoid'] <- paste(
             'Visit',
-            ifelse(queries[i,1] %in% c('SCRN', 'DM'), 1, sample(visits, 1))
+            ifelse(
+                query[1,1] %in% c('SCRN', 'DM'),
+                1,
+                sample(visits, 1)
+            )
         )
-        queries[i,8] <- sample(queryDates, 1)
-        queries[i,9] <- sample(seq(queries[i,8], as.Date('2016-12-31'), 1), 1)
-        queries[i,10] <- ifelse(
-            queries[i,3] %in% c('Closed', 'Cancelled'),
-                as.numeric(queries[i,9] - queries[i,8]),
-                as.numeric(as.Date('2016-12-31') - queries[i,8])
+        queries[i,'formoid'] <- query[1,1]
+        queries[i,'fieldname'] <- query[1,2]
+        queries[i,'markinggroup'] <- sample(markingGroups, 1, prob = markingGroupProbs)
+        queries[i,'queryopendt'] <- sample(queryDates, 1)
+        queries[i,'queryresponsedt'] <- sample(seq(queries[i,'queryopendt'], snapshotDate, 1), 1) # sample a date between query open date and the snapshot date
+        if (responses[i] < .75) {
+            queries[i,'queryresponsedt'] <- NA # simulate queries that never received a response
+            queries[i,'queryresolveddt'] <- sample(seq(queries[i,'queryopendt'], snapshotDate, 1), 1) # sample a date between query open date and the snapshot date
+        } else
+            queries[i,'queryresolveddt'] <- sample(seq(queries[i,'queryresponsedt'], snapshotDate, 1), 1) # sample a date between query response date and the snapshot date
+        if (resolvers[i] < .25)
+            queries[i,'queryresolveddt'] <- NA # simulate queries that were never resolved
+        queries[i,'querystatus'] <- case_when(
+            is.na(queries[i,'queryresponsedt']) & is.na(queries[i,'queryresolveddt']) ~ 'Open',
+            !is.na(queries[i,'queryresponsedt']) & is.na(queries[i,'queryresolveddt']) ~ 'Answered',
+            TRUE ~ sample(statuses, 1, prob = statusProbs)
         )
-        if (!queries[i,3] %in% c('Closed', 'Cancelled'))
-            queries[i,9] = NA
-        queries[i,11] <- case_when(
-            queries[i,3] %in% c('Closed', 'Cancelled') ~ queries[i,3],
-            queries[i,10] <=  28 ~ '0-4 weeks',
-            queries[i,10] <=  56 ~ '4-8 weeks',
-            queries[i,10] <= 112 ~ '8-16 weeks',
-            TRUE                 ~ '>16 weeks'
+        queries[i,'odays'] <- as.numeric(snapshotDate - queries[i,'queryopendt'])
+        queries[i,'queryrecency'] <- case_when(
+            queries[i,'odays'] <= 7 ~ '7 days',
+            queries[i,'odays'] <= 14 ~ '14 days',
+            queries[i,'odays'] <= 30 ~ '30 days',
+            TRUE ~ ''
         )
-        queries[i,12] <- as.numeric(as.Date('2016-12-31') - queries[i,8]) # pretend like it's Dec. 31 in 2016
-        queries[i,13] <- case_when(
-            queries[i,3] %in% c('Closed', 'Cancelled') ~ queries[i,3],
-            queries[i,12] <=  7 ~ '7 days',
-            queries[i,12] <=  14 ~ '14 days',
-            queries[i,12] <= 30 ~ '30 days',
-            TRUE                 ~ '> 30 days'
+        queries[i,'qdays'] <- case_when(
+            queries[i,'querystatus'] == 'Answered' ~ as.numeric(queries[i,'queryresponsedt'] - queries[i,'queryopendt']),
+            queries[i,'querystatus'] %in% c('Closed', 'Cancelled') ~ as.numeric(queries[i,'queryresolveddt'] - queries[i,'queryopendt']),
+            TRUE ~ queries[i,'odays']
         )
-
+        queries[i,'queryage'] <- case_when(
+            queries[i,'querystatus'] %in% c('Answered', 'Closed', 'Cancelled') ~ queries[i,'querystatus'],
+            queries[i,'qdays'] <=  14 ~ '0-2 weeks',
+            queries[i,'qdays'] <=  28 ~ '2-4 weeks',
+            queries[i,'qdays'] <=  56 ~ '4-8 weeks',
+            queries[i,'qdays'] <= 112 ~ '8-16 weeks',
+            TRUE ~ '>16 weeks'
+        )
     }
 
-    queries$`querytext` <- 'query text'
+    print(table(queries$querystatus))
+    print(table(queries$queryrecency))
+    print(table(queries$queryage))
+
+    queries$querytext <- 'query text'
 
     queries1 <- queries %>%
         left_join(forms) %>%
-        left_join(fields)
+        left_join(fields) %>%
+        mutate(
+            open_time = queryrecency
+        )
 
     write.csv(
         queries1,
         'queries.csv',
+        row.names = FALSE
+    )
+    write.csv(
+        queries1,
+        'dmq_Queries.csv',
         row.names = FALSE
     )
