35f8e36e9cdc94affbf174fca1e6fedebb9419b9
Spencer Childress <samussiah@gmail.com>
2019-03-25 08:13:40 -0400

    test out capture-website; need to figure out async within forEach

--- scripts/takeScreenshots.js
+++ scripts/takeScreenshots.js
@@ -1,16 +1,35 @@
-var webshot = require("webshot");
 var json = require("../data/examples.json");
+const captureWebsite = require('capture-website');
+//var webshot = require("webshot");
+
+async function asyncForEach(array, callback) {
+    for (let index = 0; index < array.length; index++) {
+        await callback(array[index], index, array);
+    }
+}
 
 json.forEach(function(repo) {
-  repo.examples.forEach(function(ex, i) {
-    webshot(
-      ex,
-      "../img/" + repo.name + "-" + i + ".png",
-      {
-        renderDelay: 5000,
-        defaultWhiteBackground: true
-      },
-      function(err) {}
-    );
-  });
+    const start = async () => {
+        await Promise.all(repo.examples.map(async (ex,i) => {
+            (async () => {
+                await captureWebsite.file(
+                    ex.example_url,
+                    '../img/' + repo.name + '-' + i + '.png'
+                );
+            })();
+        }));
+    };
+
+    start();
 });
+    //webshot(
+    //  ex.example_url,
+    //  "../img/" + repo.name + "-" + i + ".png",
+    //  {
+    //    renderDelay: 5000,
+    //    defaultWhiteBackground: true
+    //  },
+    //  function(err) {}
+    //);
+//    });
+//});
