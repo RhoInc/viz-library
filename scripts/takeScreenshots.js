3907762dd84cd8042484a2b06927b4925954c77c
jwildfire <jwildfire@gmail.com>
2019-03-26 09:24:58 -0700

    don't overwrite screenshots in build. fix #118

--- scripts/takeScreenshots.js
+++ scripts/takeScreenshots.js
@@ -1,10 +1,14 @@
 const captureWebsite = require("capture-website");
+const fs = require("fs")
 const options = {
   width: 1920,
   height: 1080,
   overwrite: true,
   delay: 3
 };
+
+const overwrite = process.argv[2]== '-o';
+console.log("Overwriting?" + overwrite)
 
 async function makeExampleList() {
   const examples = await require("../data/examples.json")
@@ -13,12 +17,19 @@ async function makeExampleList() {
       return repo.examples.map((example, i) => {
         return {
           url: example.example_url,
-          filename: `${repo.name}-${i}`
+          filename: `${repo.name}-${i}`,
         };
       }); // return's each example's URL and screenshot filename
     }) // returns each repo's example list
-    .reduce((acc, val) => acc.concat(val), []); // flattens repo array to one item per example
-
+    .reduce((acc, val) => acc.concat(val), []) // flattens repo array to one item per example
+    .filter(function(example){ //overwrite exsiting screenshots if -o flag is set
+      var path = `./img/${example.filename}.png`
+      if (fs.existsSync(path)) {
+          return overwrite ? true : false;
+        } else {
+          return true;
+        }
+    })
   return examples;
 }
 
